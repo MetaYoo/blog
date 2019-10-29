cafaa0ab5a9f8b881d5523855e98e88cc0632b2f
skyarac <haiyasheji@163.com>
2019-10-29 20:52:51 +0800

    add note

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,21 +9,22 @@ module.exports = {
         nav: [
             { text: '主页', link: '/' },
             { text: '见闻', link: '/see/' },
-            { text: '杂谈', link: '/notes/' },
+            { text: '笔记', link: '/notes/' },
+            { text: '面试', link: '/interview/' },
             // 下拉列表的配置 vuepress-theme-yubisaki
             {
                 text: '项目',
                 items: [
-                    { text: '点餐系统', link: '/project/diancan' }
+                    { text: '点餐系统', link: '/project/diancan/' }
                 ]
             },
             {
                 text: '技术',
                 items: [
-                    { text: 'C', link: '/it/c' },
-                    { text: 'Java', link: '/it/java' },
-                    { text: 'Python', link: '/it/python' },
-                    { text: '物联网', link: '/it/iot' },
+                    { text: 'C', link: '/it/c/' },
+                    { text: 'Java', link: '/it/java/' },
+                    { text: 'Python', link: '/it/python/' },
+                    { text: '物联网', link: '/it/iot/' },
                 ]
             }
         ]
---
+++ docs/it/java/spring/2018-10-01-springboot源码解析.md
@@ -0,0 +1,7 @@
+---
+layout: post
+title:  "SpringBoot源码解析"
+date:   2018-10-01 18:00
+categories: SpringBoot源码解析
+permalink: /archivers/springboot-1
+---
